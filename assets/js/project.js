acef01445b335a17300c4a6fde143e76295c0216
Jacob Horner <horner.jacob11@gmail.com>
2023-05-11 19:19:22 -0500

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,9 +1,14 @@
 function generateSound() {
-  alert("Someday we will make a noise!");
-  
+
   //create a synth and connect it to the main output (your speakers)
   const synth1 = new Tone.Synth().toDestination();
   
   //play a middle 'C' for the duration of an 8th note
-  synth1.triggerAttackRelease("C4", "8n");
+  //synth1.triggerAttackRelease("C4", "8n");
+  synth.triggerAttack("D4", now);
+  synth.triggerAttack("F4", now + 0.5);
+  synth.triggerAttack("A4", now + 1);
+  synth.triggerAttack("C5", now + 1.5);
+  synth.triggerAttack("E5", now + 2);
+  synth.triggerRelease(["D4", "F4", "A4", "C5", "E5"], now + 4);
 }
