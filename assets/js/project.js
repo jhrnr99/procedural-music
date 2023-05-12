aa42605e192ef2d487fe40a219116840079e256a
Jacob Horner <horner.jacob11@gmail.com>
2023-05-11 19:03:15 -0500

    Update project.js

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,11 +1,12 @@
-import * as Tone from 'tone'
+var MonoSynth = require("Tone").MonoSynth;
+var synth = new MonoSynth();
 
 function generateSound() {
   alert("Someday we will make a noise!");
   
   //create a synth and connect it to the main output (your speakers)
-  const synth = new Tone.Synth().toDestination();
+  const synth1 = new Tone.Synth().toDestination();
   
   //play a middle 'C' for the duration of an 8th note
-  synth.triggerAttackRelease("C4", "8n");
+  synth1.triggerAttackRelease("C4", "8n");
 }
